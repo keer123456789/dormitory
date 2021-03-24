6958f7e3825cd32e3bfabe18636ad637ac52d13b
keer <1073441240@qq.com>
2021-03-24 23:32:31 +0800

    server add poi dependence for excel add model file for student and block

---
+++ server/src/main/java/com/keer/dormitory/constant/ExeclConstant.java
@@ -0,0 +1,48 @@
+package com.keer.dormitory.constant;
+
+/**
+ * execl 模板中使用的常量
+ */
+public class ExeclConstant {
+    public static final String CELL_NUM = "序号";
+    public static final int CELL_NUM_LOCATION = 0;
+    /**
+     * 宿舍楼导入模板中execl中常量
+     */
+    public static final String SHEET_BLOCKINFO = "宿舍楼信息";
+    public static final String SHEET_ROOMINFO = "号楼房间信息";
+
+    public static final String CELL_BLOCK = "宿舍楼名字";
+    public static final int CELL_BLOCK_LOCATION = 1;
+    public static final String CELL_ROOM_SIZE = "宿舍大小（几人间）";
+    public static final int CELL_ROOM_SIZE_LOCATION = 2;
+    public static final String CELL_FlOOR_NUM = "楼层数";
+    public static final int CELL_FlOOR_NUM_LOCATION = 3;
+
+    public static final String CELL_ROOM_NAME = "房间名称";
+    public static final int CELL_ROOM_NAME_LOCATION = 1;
+    public static final String CELL_ROOM_FLOOR = "房间楼层";
+    public static final int CELL_ROOM_FLOOR_LOCATION = 2;
+
+    public static final String CELL_STUDENT_NAME="学生姓名";
+    public static final int CELL_STUDENT_NAME_LOCATION=1;
+    public static final String CELL_IDENTITY="身份证号";
+    public static final int CELL_IDENTITY_LOCATION=2;
+    public static final String CELL_NATION="民族";
+    public static final int CELL_NATION_LOCATION=3;
+    public static final String CELL_SEX="性别";
+    public static final int CELL_SEX_LOCATION=4;
+    public static final String CELL_ACADEMY="学院";
+    public static final int CELL_ACADEMY_LOCATION=5;
+    public static final String CELL_MAJOR="专业";
+    public static final int CELL_MAJOR_LOCATION=6;
+    public static final String CELL_CLASS="行政班级号";
+    public static final int CELL_CLASS_LOCATION=7;
+    public static final String CELL_REGION="地区";
+    public static final int CELL_REGION_LOCATION=8;
+    public static final String CELL_PHONE="手机号";
+    public static final int CELL_PHONE_LOCATION=9;
+    public static final String CELL_ADD="家庭住址";
+    public static final int CELL_ADD_LOCATION=10;
+
+}
---
+++ server/src/main/java/com/keer/dormitory/util/ExcelUtil.java
@@ -0,0 +1,114 @@
+package com.keer.dormitory.util;
+
+import com.keer.dormitory.constant.ExeclConstant;
+import org.apache.poi.ss.usermodel.*;
+
+import java.io.File;
+import java.io.IOException;
+
+public class ExcelUtil {
+    public static void main(String[] args) throws Exception {
+
+        System.out.println(checkExcelBlockModel("file/宿舍楼导入模板.xlsx"));
+        System.out.println(checkExcelStudentModel("file/学生信息导入模板.xlsx"));
+
+    }
+
+    /**
+     * 检查宿舍楼信息导入文件格式是否正确
+     *
+     * @param path 文件路径
+     * @return
+     */
+    public static boolean checkExcelBlockModel(String path) {
+        Workbook wb = null;
+        try {
+            wb = WorkbookFactory.create(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Sheet block = wb.getSheet(ExeclConstant.SHEET_BLOCKINFO);
+        if (block == null) {
+            return false;
+        }
+        Row r = block.getRow(0);
+        if (!r.getCell(ExeclConstant.CELL_NUM_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_NUM)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_BLOCK_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_BLOCK)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_ROOM_SIZE_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_ROOM_SIZE)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_FlOOR_NUM_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_FlOOR_NUM)) {
+            return false;
+        }
+        System.out.println(block.getLastRowNum());
+        for (int i = 1; i <= block.getLastRowNum(); i++) {
+            Sheet room = wb.getSheet(i + ExeclConstant.SHEET_ROOMINFO);
+            if (room == null) {
+                return false;
+            }
+            Row info = room.getRow(0);
+            if (!info.getCell(ExeclConstant.CELL_NUM_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_NUM)) {
+                return false;
+            }
+            if (!info.getCell(ExeclConstant.CELL_ROOM_NAME_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_ROOM_NAME)) {
+                return false;
+            }
+            if (!info.getCell(ExeclConstant.CELL_ROOM_FLOOR_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_ROOM_FLOOR)) {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public static boolean checkExcelStudentModel(String path) {
+        Workbook wb = null;
+        try {
+            wb = WorkbookFactory.create(new File(path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        Sheet student = wb.getSheetAt(0);
+        if (student == null) {
+            return false;
+        }
+        Row r = student.getRow(0);
+        if (!r.getCell(ExeclConstant.CELL_NUM_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_NUM)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_STUDENT_NAME_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_STUDENT_NAME)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_IDENTITY_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_IDENTITY)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_NATION_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_NATION)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_SEX_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_SEX)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_ACADEMY_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_ACADEMY)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_MAJOR_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_MAJOR)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_CLASS_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_CLASS)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_REGION_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_REGION)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_PHONE_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_PHONE)) {
+            return false;
+        }
+        if (!r.getCell(ExeclConstant.CELL_ADD_LOCATION).getStringCellValue().equals(ExeclConstant.CELL_ADD)) {
+            return false;
+        }
+        return true;
+    }
+}
